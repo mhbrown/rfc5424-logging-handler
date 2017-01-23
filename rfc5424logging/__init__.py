068babfa41a46374d44bd8ecadb218cdd5f25439
Martin Brown <Martin.Brown@viasat.com>
2017-01-23 11:17:47 -0800

    Revert "Added a custom logger to easily pass msgid and structured_data as parameters"
    This reverts commit 381d59a01c1c9a5497b161d7aa19b06052a750af.

--- rfc5424logging/__init__.py
+++ rfc5424logging/__init__.py
@@ -1,4 +1,3 @@
 from .handler import Rfc5424SysLogHandler
-from .logger import Rfc5424SysLogLogger
 
-__all__ = ['Rfc5424SysLogHandler', 'Rfc5424SysLogLogger', ]
+__all__ = ['Rfc5424SysLogHandler', ]
